60ca7f32b7220db145edf3fbf3f220c202d799d4
Danil <mydogfromhell666@gmail.com>
2023-06-19 18:34:37 +0300

    add query search

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ import cors from 'cors'
 const app = express()
 const port = 5000
 
-app.get('/api/products', cors(), (req, res) => {
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/api/products/all', cors(), (req, res) => {
   res.json(products)
 })
 
-app.use(express.json())
-app.use(express.static('public'))
+app.get('/api/products/find', cors(), (req, res) => {
+  const searchQuery = req.query.desc.replace(/"/g, '');
+  const result = products.filter(el => el.description.includes(searchQuery));
+  res.json(result);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
