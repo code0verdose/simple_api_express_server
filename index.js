83a6061801003635ec6af0b01545a4c75262272e
root <root@vm2146178.firstbyte.club>
2023-06-19 16:05:14 +0300

    add forever/cors lib

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import { products } from './data/products/cardsData.js'
 import express from 'express'
+import cors from 'cors'
 const app = express()
 const port = 5000
 
-app.get('/api/products', (req, res) => {
+app.get('/api/products', cors(), (req, res) => {
   res.json(products)
 })
 
+app.use(express.json())
 app.use(express.static('public'))
 
 app.listen(port, () => {
