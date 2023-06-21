9e2bdae3740ba1c104c85c82b61f90b6591a8a22
Danil <mydogfromhell666@gmail.com>
2023-06-21 12:23:30 +0300

    add emoji endpoint / data

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import { products } from './data/products/cardsData.js'
+import {emoji} from "./data/emoji/emoji.js";
+import {emojiHard} from "./data/emojiHard/emojiHard.js";
 import express from 'express'
 import cors from 'cors'
 const app = express()
-const port = 5000
+const port = 7000
 
 app.use(express.json())
 app.use(express.static('public'))
+
+app.get('/api/emoji/v1', cors(), (req, res) => {
+  res.json(emoji)
+})
+
+app.get('/api/emoji/v2', cors(), (req, res) => {
+  res.json(emojiHard)
+})
 
 app.get('/api/products/all', cors(), (req, res) => {
   res.json(products)
